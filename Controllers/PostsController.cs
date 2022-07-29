2a994f9027dfd7f41b1d6773cce1dfd1b7ecce7a
Elif Sude Bulut <c1928007@student.cankaya.edu.tr>
2022-07-29 17:12:07 +0200

    Add project files.

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheDevBlog.API.Data;
+using TheDevBlog.API.Models.DTO;
+using TheDevBlog.API.Models.Entities;
+
+namespace TheDevBlog.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        private readonly TheDevBlogDbContext dbContext;
+
+        public PostsController(TheDevBlogDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // WHERE THE CRUD OPERATIONS BEGIN
+
+        // Read -> List all posts
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPosts()
+        {
+            var posts = await dbContext.Posts.ToListAsync();
+
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetPostById")]
+        public async Task<IActionResult> GetPostById(Guid id)
+        {
+            var post = await dbContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post != null)
+            {
+                return Ok(post);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(AddPostRequest addPostRequest)
+        {
+            // convert DTO to entity 
+
+            var post = new Post()
+            {
+                Title = addPostRequest.Title,
+                Content = addPostRequest.Content,
+                Summary = addPostRequest.Summary,
+                UrlHandle = addPostRequest.UrlHandle,
+                Author = addPostRequest.Author,
+                FeaturedImageUrl = addPostRequest.FeaturedImageUrl,
+                Visible = addPostRequest.Visible,
+                PublishedDate = addPostRequest.PublishedDate,
+                UpdatedDate = addPostRequest.UpdatedDate
+            };
+
+            post.Id = Guid.NewGuid();
+            await dbContext.Posts.AddAsync(post);
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPostById), new { id = post.Id }, post);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdatePost([FromRoute] Guid id, UpdatePostRequest updatePostRequest)
+        {
+
+            // Check if exists
+            var existingPost = await dbContext.Posts.FindAsync(id);
+
+            if (existingPost != null)
+            {
+                existingPost.Title = updatePostRequest.Title;
+                existingPost.Content = updatePostRequest.Content;
+                existingPost.Summary = updatePostRequest.Summary;
+                existingPost.UrlHandle = updatePostRequest.UrlHandle;
+                existingPost.Author = updatePostRequest.Author;
+                existingPost.FeaturedImageUrl = updatePostRequest.FeaturedImageUrl;
+                existingPost.Visible = updatePostRequest.Visible;
+                existingPost.PublishedDate = updatePostRequest.PublishedDate;
+                existingPost.UpdatedDate = updatePostRequest.UpdatedDate;
+
+                await dbContext.SaveChangesAsync();
+                return Ok(existingPost);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeletePost(Guid id)
+        {
+            var existingPost =  await dbContext.Posts.FindAsync(id);
+
+            if (existingPost != null)
+            {
+                dbContext.Remove(existingPost);
+                await dbContext.SaveChangesAsync();
+                return Ok(existingPost);
+            }
+
+            return NotFound();
+        }
+    } 
+}
---
+++ Data/TheDevBlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheDevBlog.API.Models.Entities;
+
+namespace TheDevBlog.API.Data
+{
+    public class TheDevBlogDbContext : DbContext
+    {
+        public TheDevBlogDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // DBSet
+        public DbSet<Post> Posts { get; set; }
+    }
+}
